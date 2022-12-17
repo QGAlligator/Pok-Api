caafd40a974224ca76d6e0fef8380b7d5addb2bf
QGAlligator <94532575+QGAlligator@users.noreply.github.com>
2022-12-17 15:14:58 +0100

    Add types and cards for Pokm

--- src/App.js
+++ src/App.js
@@ -1,68 +1,15 @@
 import "./App.css";
-import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
 import Logo from "./img/Logo.png";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Accueil from "./components/Accueil";
-import Pokedex from "./components/MonPokedex";
-import MonPokedex from "./components/Pokedex";
+import Pokedex from "./components/Pokedex";
+import MonPokedex from "./components/MonPokedex";
 
 export default function App() {
-  const [pokemons, setPokemons] = useState([]);
-  const [image, setImage] = useState("");
-  const [url, setUrl] = useState({
-    current: "https://pokeapi.co/api/v2/pokemon/",
-    next: null,
-    previous: null,
-  });
-
-  const next = () => {
-    const newUrl = {
-      current: url.next,
-      previous: url.current,
-      next: null,
-    };
-    setUrl(newUrl);
-  };
-  const previous = () => {
-    const newUrl = {
-      current: url.previous,
-      previous: null,
-      next: url.current,
-    };
-    setUrl(newUrl);
-  };
-
-  useEffect(() => {
-    fetch(url.current)
-      .then((res) => res.json())
-      .then((data) => {
-        setPokemons(data.results);
-        setUrl({
-          current: url.current,
-          next: data.next,
-          previous: data.previous,
-        });
-        console.log(data);
-      })
-      .catch((err) => console.error(err));
-    //eslint-disable-next-line
-  }, [url.current]);
-
-  useEffect(() => {
-    const pokemon = "charizard";
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setImage(data.sprites.front_default);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-
   return (
     <>
       <Navbar bg="danger" variant="light">
@@ -71,12 +18,19 @@ export default function App() {
             <Image src={Logo} alt="PokeApi Logo" className="App-logo" />
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="Accueil">Accueil</Nav.Link>
-            <Nav.Link href="Pokedex">Pokédex</Nav.Link>
-            <Nav.Link href="Mon-Pokedex">Mon Pokédex</Nav.Link>
+            <Nav.Link href="Accueil" className="navl">
+              Accueil
+            </Nav.Link>
+            <Nav.Link href="Pokedex" className="navl">
+              Pokédex
+            </Nav.Link>
+            <Nav.Link href="Mon-Pokedex" className="navl">
+              Mon Pokédex
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      <div className="border"></div>
       <Container>
         <Routes>
           <Route path="/" element={<Navigate to="/Accueil" />} />
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,12 +1,130 @@
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { useEffect, useState } from "react";
 
 export default function MonPokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  const [type1, setType1] = useState("");
+  const [type2, setType2] = useState("");
+  const [url, setUrl] = useState({
+    current: "https://pokeapi.co/api/v2/pokemon/",
+    next: null,
+    previous: null,
+  });
+
+  const next = () => {
+    const newUrl = {
+      current: url.next,
+      previous: url.current,
+      next: null,
+    };
+    setUrl(newUrl);
+  };
+  const previous = () => {
+    const newUrl = {
+      current: url.previous,
+      previous: null,
+      next: url.current,
+    };
+    setUrl(newUrl);
+  };
+
+  useEffect(() => {
+    fetch(url.current)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemons(data.results);
+        setUrl({
+          current: url.current,
+          next: data.next,
+          previous: data.previous,
+        });
+      })
+      .catch((err) => console.error(err));
+    //eslint-disable-next-line
+  }, [url.current]);
+
+  useEffect(() => {
+    pokemons.map((pokemon) => {
+      fetch(pokemon.url)
+        .then((res) => res.json())
+        .then((data) => {
+          setType1((current) => [...current, data.types[0].type.name]);
+          setType2((current) => [...current, data.types[1]?.type?.name]);
+        })
+        .then(setType1(""))
+        .then(setType2(""))
+        .catch((err) => console.error(err));
+      return null;
+    });
+  }, [pokemons]);
+
   return (
-    <Container>
-      <Card body>
-        <h2>mon pokedex</h2>
-      </Card>
-    </Container>
+    <div>
+      <Container>
+        {pokemons.map((pokemon) => (
+          <Card className="cards">
+            <Card.Header className="pkmn bg-danger">
+              {"#" + pokemon.url.split("/")[6] + " " + pokemon.name}
+            </Card.Header>
+            <img
+              className="pkimg"
+              src={
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+                pokemon.url.split("/")[6] +
+                ".png"
+              }
+              alt={"Image de " + pokemon.name}
+            />
+            <Container className="bg-danger">
+              <Card
+                className={
+                  type1[
+                    pokemon.url.split("/")[6] % 20 === 0
+                      ? 19
+                      : (pokemon.url.split("/")[6] % 20) - 1
+                  ] + " cardsp"
+                }
+              >
+                {
+                  type1[
+                    pokemon.url.split("/")[6] % 20 === 0
+                      ? 19
+                      : (pokemon.url.split("/")[6] % 20) - 1
+                  ]
+                }
+              </Card>
+              {type2[
+                pokemon.url.split("/")[6] % 20 === 0
+                  ? 19
+                  : (pokemon.url.split("/")[6] % 20) - 1
+              ] ? (
+                <Card
+                  className={
+                    type2[
+                      pokemon.url.split("/")[6] % 20 === 0
+                        ? 19
+                        : (pokemon.url.split("/")[6] % 20) - 1
+                    ] + " cardsp"
+                  }
+                >
+                  {
+                    type2[
+                      pokemon.url.split("/")[6] % 20 === 0
+                        ? 19
+                        : (pokemon.url.split("/")[6] % 20) - 1
+                    ]
+                  }
+                </Card>
+              ) : null}
+            </Container>
+          </Card>
+        ))}
+      </Container>
+      <div className="border"></div>
+      {url.previous && <Button onClick={previous}>Previous</Button>}
+      {url.next && <Button onClick={next}>Next</Button>}
+    </div>
   );
 }
